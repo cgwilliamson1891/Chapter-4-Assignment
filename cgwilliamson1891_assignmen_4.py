ef517760bb835ff55e4839eb1c72943394c8ad55
Chloe Williamson <chloewilliamson@MacBookPro.ncat.edu>
2026-02-10 21:19:16 -0500

    Add initial game setup with required variable

---
+++ cgwilliamson1891_assignmen_4.py
@@ -0,0 +1,20 @@
+# COMP 163 - Intro to Programming
+# Assignment: Chapter 4 - College Life Adventure Game
+# Name: Chloe Williamson
+# GitHub Username: cgwilliamson1891
+# Date:
+# Description:  [2-3 sentences describing your game's theme and objective] 
+# AI Usage:
+
+student_name = "Chloe Williamson"
+current_gpa = 3.7
+study_hours = 25
+social_points = 50
+stress_level = 70
+
+print(f"Welcome, {student_name}!")
+print(f"Starting GPA: {current_gpa}")
+print(f"Study Hours: {study_hours}")
+print(f"Social Points: {social_points}")
+print(f"Stress Level: {stress_level}")
+
